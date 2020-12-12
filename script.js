040ab97f6769d72cae3962ee671e0a37919fb42f
shreykoradia <67724256+shreykoradia@users.noreply.github.com>
2020-12-12 15:29:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+(function () {
+    function checkTime(i) {
+        return (i < 10) ? "0" + i : i;
+    }
+
+    function startTime() {
+        var today = new Date(),
+            h = checkTime(today.getHours()),
+            m = checkTime(today.getMinutes()),
+            s = checkTime(today.getSeconds());
+        document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+        t = setTimeout(function () {
+            startTime()
+        }, 500);
+    }
+    startTime();
+})();
+
+
+
+
 //selectors
 const todoButton = document.querySelector(".todo-button");
 const todoInput = document.querySelector(".todo-input");
